25d1f662fd8c55251f6abe7a56b8ef47be9fd7d7
Benny <benny.taccardi@gmail.com>
2019-03-20 12:00:11 +0100

    first

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, ChangeDetectorRef} from '@angular/core';
+import {NavController, Platform, Events} from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  beaconData: any;
+  constructor(private platform: Platform, private change: ChangeDetectorRef) {
+  }
+  startScanningForBeacons() {
+    this.platform.ready().then(() => {
+      evothings.eddystone.startScan((data) => {
+        this.beaconData = data;
+        console.log(this.beaconData);
+        setTimeout(() => {
+          this.change.detectChanges();
+        }, 1000);
+      }, error => console.error(error));
+    });
+  }
 }
